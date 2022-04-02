b0ab7b5f13d07bf3c88a4153eed7042824fc6189
CT <chris@toxz.nl>
2022-04-02 23:55:46 +0200

    app layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,31 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Dashboard</title>
-        <link href="css/app.css" rel="stylesheet">
-        <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body class="antialiased bg-background-surface font-brand font-light">
-        @if (Route::has('login'))
-            <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                        @auth
-                            <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                        @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+@extends('layouts.app')
 
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                            @endif
-                        @endauth
-                    </div>
-            </div>
-        @endif
-        
-        <div class="flex flex-row"><!-- Main wrapper -->
-
+@section('content')
             <div class="flex justify-center w-16 h-screen bg-background-secondary"> <!-- Sidebar -->
                 <ul class="flex flex-col h-1/3 mt-32">
                     <li class="bg-background-tertiary w-10 h-10 mb-1 rounded-full flex justify-center items-center">
@@ -45,7 +20,7 @@
                     </li>
                 </ul>
             </div><!-- End Sidebar -->
- 
+
             <div class="flex flex-col basis-full"><!-- content area -->
 
                 <header class="flex h-20 flex-row justify-between mt-5 mx-8"><!-- Header -->
@@ -102,6 +77,4 @@
                     </footer>
                 </div><!-- End Footer --> --}}
 
-        </div>
-    </body>
-</html>
+@endsection
