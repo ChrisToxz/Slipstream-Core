2e509af329c284fb2ddd83e1dca381323be3e955
Randell b <58916132+randelldb@users.noreply.github.com>
2022-04-04 23:52:43 +0200

    Fixing responsive issues, menu

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+            {{-- Dev --}}
+                <div class="absolute bottom-0 right-0 w-10 h-10 2xl:bg-red-500 xl:bg-orange-500 lg:bg-yellow-500 md:bg-green-500 sm:bg-blue-500"></div>
+            {{-- Dev --}}
             <div class="flex justify-center w-16 h-screen bg-background-secondary"> <!-- Sidebar -->
                 <ul class="flex flex-col h-1/3 mt-32">
                     <li class="bg-background-tertiary w-10 h-10 mb-1 rounded-full flex justify-center items-center">
@@ -25,8 +28,8 @@
 
                 <header class="flex h-20 flex-row justify-between mt-5 mx-8"><!-- Header -->
                     <div class="self-center"><img src="media/logo.png" alt=""></div>
-                    <nav class="basis-1/6 bg-background-secondary rounded-md self-center text-base text-gray-200 py-2">
-                        <ul class="flex flex-row justify-around">
+                    <nav class="basis-1/6 bg-background-secondary rounded-md self-center text-base text-gray-200 py-2 px-4">
+                        <ul class="flex justify-around gap-8 sm:gap-2">
                             <li class="transition-all cursor-pointer hover:text-brand-primary-500">Dashboard</li>
                             <li class="transition-all cursor-pointer hover:text-brand-primary-500">Settings</li>
                             <li class="transition-all cursor-pointer hover:text-brand-primary-500">Logout</li>
@@ -34,7 +37,7 @@
                     </nav>
                 </header>
 
-                <div class="grid gap-8 self-center mx-20 mt-8 grid-cols-3"><!-- Videocards -->
+                <div class="grid gap-8 self-center mx-20 mt-8 lg:grid-cols-3 md:grid-cols-2"><!-- Videocards -->
                     <x-videocard message="media/stock1.jpg" />
                     <x-videocard message="media/stock2.jpg" />
                     <x-videocard message="media/stock3.jpg" />
