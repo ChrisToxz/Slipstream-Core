4060c1b2f2803f2185524e986b40af84215b97e8
CT <chris@toxz.nl>
2022-04-10 17:08:21 +0200

    video upload

--- app/Http/Livewire/Media/Upload.php
+++ app/Http/Livewire/Media/Upload.php
@@ -4,8 +4,10 @@ namespace App\Http\Livewire\Media;
 
 use App\Helpers;
 use App\Models\Tag;
+use App\Models\Video;
 use Livewire\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class Upload extends ModalComponent
 {
@@ -35,6 +37,31 @@ class Upload extends ModalComponent
             'title' => $title,
             'description' => $this->description,
         ]);
+
+        if(Helpers\Media::isVideo($this->media->path())){
+            $ffmpeg = FFMpeg::openUrl($this->media->path());
+
+            $media = Video::create([
+                'original' => $hash,
+                'info'      => [
+                    'size'              => round($this->media->getSize()/1000000), // to MB
+                    'extension'         => $this->media->extension(),
+                    'codec_name'        => $ffmpeg->getVideoStream()->get('codec_name'),
+                    'codec_long_name'   => $ffmpeg->getVideoStream()->get('codec_long_name'),
+                    'bit_rate'          => $ffmpeg->getVideoStream()->get('bit_rate'),
+                    'width'             => $ffmpeg->getVideoStream()->get('width'),
+                    'height'            => $ffmpeg->getVideoStream()->get('height'),
+                    'r_frame_rate'      => $ffmpeg->getVideoStream()->get('r_frame_rate'),
+                    'avg_frame_rate'    => $ffmpeg->getVideoStream()->get('avg_frame_rate'),
+                    'tags'              => $ffmpeg->getVideoStream()->get('tags'),]
+            ]);
+        }
+
+        if(Helpers\Media::isImage($this->media->path())){
+
+        }
+
+        $media->tag()->save($tag);
     }
 
     public function render()
