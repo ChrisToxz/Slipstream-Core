48cee19eaf746ba704ac39ec6eac04c06be139fa
ChrisToxz <chris@toxz.nl>
2023-06-27 22:08:20 +0200

    Added Laravel Enum

--- routes/web.php
+++ routes/web.php
@@ -16,28 +16,30 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return inertia('Dashboard');
-});
 
-
-Route::get('/a', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+ROute::get('/', [\App\Http\Controllers\SlipController::class, 'index']);
+Route::resource('slip', \App\Http\Controllers\SlipController::class)->except('index');
 
 require __DIR__.'/auth.php';
+
+/* not for now */
+//Route::get('/a', function () {
+//    return Inertia::render('Welcome', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+//});
+//
+//Route::get('/dashboard', function () {
+//    return Inertia::render('Dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+//
+//Route::middleware('auth')->group(function () {
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
+
+
