b813f7d06b71e33fd1b113a6049ead341ddf59cf
DamoFD <damionvoshall@gmail.com>
2023-06-28 11:59:02 -0700

    button animations, select options

--- resources/js/Components/UploadSettings.vue
+++ resources/js/Components/UploadSettings.vue
@@ -9,10 +9,14 @@
     <label class="mt-4" for="description">Description</label>
     <textarea id="description" class="h-[300px] bg-[#1A1A1A] border-[#0580c5] rounded-lg" />
     <label class="mt-4" for="type">Type</label>
-    <select id="type" class="bg-[#1A1A1A] border-[#0580c5] rounded-lg" />
+    <select id="type" class="bg-[#1A1A1A] border-[#0580c5] rounded-lg">
+      <option value="1">None (Original File)</option>
+      <option value="2">Optimized for web (x264)</option>
+      <option value="3">Optimized for streaming (x264/HLS)</option>
+    </select>
     <div class="flex mt-4 justify-between">
-      <button class="bg-[#0580c5] py-2 px-6 rounded-lg">Submit</button>
-      <button class="bg-red-800 py-2 px-6 rounded-lg">Cancel</button>
+      <button class="bg-[#0580c5] py-2 px-6 rounded-lg transform hover:-translate-y-1 hover:bg-[#035e91] transition-all duration-200">Submit</button>
+      <button class="bg-red-800 py-2 px-6 rounded-lg transform hover:-translate-y-1 hover:bg-red-900 transition-all duration-200">Cancel</button>
     </div>
   </div>
 </template>
