53771e2e7ae5fa5a1acf21d68922920774c2ba37
DamoFD <damionvoshall@gmail.com>
2023-06-28 13:28:41 -0700

    env example db update

--- resources/js/Components/VideoPreview.vue
+++ resources/js/Components/VideoPreview.vue
@@ -1,15 +1,64 @@
 <script setup>
+import { ref, reactive } from 'vue'
 import Play from '~icons/ion/play'
+
+const form = reactive({
+  text: null,
+  file: null,
+})
+
+let isValidFile = ref(null)
+let fileDisplay = ref('')
+let tite = ref('')
+let description = ref('')
+let type = ref('')
+let error = ref({
+  text: null,
+  file: null,
+})
+
+const getUploadedFile = (e) => {
+  form.file = e.target.files[0]
+  let extension = form.file.name.substring(form.file.name.lastIndexOf('.') + 1)
+
+  if (extension == 'mp4') {
+    isValidFile.value = true
+  } else {
+    isValidFile.value = false
+    return
+  }
+
+  fileDisplay.value = URL.createObjectURL(e.target.files[0])
+}
 </script>
 
 <template>
-  <div class="w-[710px] ml-[130px] flex flex-col justify-center relative pt-10">
+  <div v-if="!fileDisplay" class="w-[710px] h-full ml-[130px] flex flex-col justify-center items-center pt-10">
+    <label for="file" class="text-gray-200">
+      Select From Computer
+    </label>
+    <input
+      id="file"
+      type="file"
+      class="hidden"
+      @input="$event => getUploadedFile($event)"
+    />
+    <p v-if="error && error.file">
+      {{ error.file }}
+    </p>
+    <p v-if="!fileDisplay && isValidFile === false">
+      File not accepted
+    </p>
+  </div>
+
+
+  <div v-if="fileDisplay && isValidFile === true" class="w-[710px] ml-[130px] flex flex-col justify-center relative pt-10">
     <div class="w-full relative">
       <div class="bg-[rgba(5,128,197,0.6)] rounded-full w-[38px] h-[38px] flex items-center justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <Play color="white" />
       </div>
       <div class="w-full h-[462px]">
-        <img class="w-full rounded-lg" src="/img/thumbnail.jpg" alt="thumbnail" />
+        <video controls class="w-full rounded-lg" :src="fileDisplay" alt="thumbnail" />
       </div>
     </div>
     <div class="w-full h-[26px] bg-[#0a0a0a] rounded-2xl mt-4 flex justify-center items-center">
