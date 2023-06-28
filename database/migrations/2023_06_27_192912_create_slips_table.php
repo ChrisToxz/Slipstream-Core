298b54be49849a17510272817aa4816635b99ab0
ChrisToxz <chris@toxz.nl>
2023-06-28 20:40:37 +0200

    Adjust Slips model temporarily to be nullable

--- database/migrations/2023_06_27_192912_create_slips_table.php
+++ database/migrations/2023_06_27_192912_create_slips_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +16,8 @@ return new class extends Migration
             $table->string('title');
             $table->string('description')->nullable();
             $table->string('thumb');
-            $table->morphs('mediable');
+            // TODO: Make it not nullable, just for testing
+            $table->nullableMorphs('mediable');
 
             $table->timestamps();
         });
