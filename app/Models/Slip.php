46b3f439ceb39432a9dc5191c0697396825d7cf6
ChrisToxz <chris@toxz.nl>
2023-06-30 23:33:50 +0200

    Generate automatticly a token when creating a new Slip model

--- app/Models/Slip.php
+++ app/Models/Slip.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Str;
 
 /**
  * App\Models\Slip
@@ -34,4 +35,26 @@ class Slip extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'thumb'];
+
+    public function getRouteKeyName()
+    {
+        return 'token';
+    }
+
+//    public static function create(array $attributes = []): Model|\Illuminate\Database\Eloquent\Builder
+//    {
+//        // TODO: improve, make sure its unique too.
+//        $attributes['token'] = Str::random(6);
+//
+//        $model = static::query()->create($attributes);
+//
+//        return $model;
+//    }
+    public static function booted()
+    {
+        static::creating(function ($slip) {
+            $slip->token = Str::random(6);
+        });
+    }
+
 }
