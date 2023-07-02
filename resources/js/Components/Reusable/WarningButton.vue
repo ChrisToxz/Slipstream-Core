d37bd40d77eb15f345e3693682860b7e51c8442e
DamoFD <damionvoshall@gmail.com>
2023-07-02 11:38:33 -0700

    button animations and color changes

--- resources/js/Components/Reusable/WarningButton.vue
+++ resources/js/Components/Reusable/WarningButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="bg-red-800 py-2 w-full rounded-lg hover:bg-red-900">
+  <button class="bg-red-800 py-2 w-full rounded-lg transition-all duration-300 ease hover:-translate-y-0.5 hover:bg-red-900">
     <slot />
   </button>
 </template>
