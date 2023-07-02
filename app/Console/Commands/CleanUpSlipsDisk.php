f0e8813f8db336bff55e61d3defdea163384dd82
ChrisToxz <chris@toxz.nl>
2023-07-02 15:31:28 +0200

    ss:cleanslips command to clean all uploaded slips

---
+++ app/Console/Commands/CleanUpSlipsDisk.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class CleanUpSlipsDisk extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ss:cleanslips';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete all `slips` in Slips file disk';
+
+    /**
+     * Execute the console command.
+     */
+
+    // TODO: Check if Slip contains in DB as well, and remove
+    public function handle()
+    {
+        $disk = Storage::disk('slips');
+
+        $dirs = $disk->directories();
+
+        $deleted = 0;
+        foreach ($dirs as $dir) {
+            $disk->deleteDirectory($dir);
+            $this->info('Deleted directory: ' . $dir);
+            $deleted++;
+        }
+
+        $this->info('Disk cleanup completed, deleted ' . $deleted . ' directories');
+    }
+}
