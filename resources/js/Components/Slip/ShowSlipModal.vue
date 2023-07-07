4f3a1107faa88a1994da0dfd1d9ed0fd1c1bef8f
ChrisToxz <chris@toxz.nl>
2023-07-08 01:06:42 +0200

    HLS Player development

--- resources/js/Components/Slip/ShowSlipModal.vue
+++ resources/js/Components/Slip/ShowSlipModal.vue
@@ -1,8 +1,8 @@
 <script setup>
 
-import {ref, onMounted} from 'vue'
-// import Plyr from 'plyr'
-// import Hls from 'hls.js'
+import {ref, onMounted, reactive} from 'vue'
+import Plyr from 'plyr'
+import Hls from 'hls.js'
 
 const props = defineProps({
   slip: Object,
@@ -10,35 +10,70 @@ const props = defineProps({
 // worked, since no Hls is undefined errors.
 // lets try Plyr as well.
 // so its not version related.
-const source = props.slip.mediable.path
 const video = ref(null)
 const defaultOptions = {}
 const videosource = ref(null)
+const source = reactive(props.slip.mediable.path)
+
 
 onMounted(() => {
-  if (Hls.isSupported()) {
+  if (!Hls.isSupported() || props.slip.mediable.type != 3) {
+    console.log('HLS is not supported')
+    Plyr.setup(video, defaultOptions)
+    // Autoplay not working
+    video.value.play()
+
+  } else if(Hls.isSupported()) {
     // hmm, anyway the consolelog here make sense, because nothing have been done with video yet?
     console.log('Hls is supported')
-    var hls = new Hls()
-    hls.loadSource('http://localhost/storage/slips/72y9Ko/tclOw4bIPAdqgNljkjfujidIx9NBGTnuCM4glo18.m3u8')
+    const hls = new Hls()
+    hls.loadSource(source)
+
+    hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+
+      // Transform available levels into an array of integers (height values).
+      const availableQualities = hls.levels.map((l) => l.height)
+      console.log(availableQualities)
+
+      // Add new qualities to option
+      defaultOptions.quality = {
+        default: availableQualities[0],
+        options: availableQualities,
+        // this ensures Plyr to use Hls to update quality level
+        forced: true,
+        onChange: (e) => updateQuality(e),
+      }
+
+      // Initialize here
+      new Plyr(video, defaultOptions)
+    })
+
     hls.attachMedia(video.value)
-    console.log(video.value)
-    hls.on(Hls.Events.MANIFEST_PARSED,function() {
-      video.value.play()
+    window.hls = hls
+
+
+
+  }
+})
+function updateQuality(newQuality) {
+  if (newQuality === 0) {
+    window.hls.currentLevel = -1 //Enable AUTO quality if option.value = 0
+  } else {
+    window.hls.levels.forEach((level, levelIndex) => {
+      if (level.height === newQuality) {
+        console.log('Found quality match with ' + newQuality)
+        window.hls.currentLevel = levelIndex
+      }
     })
   }
+}
 
-  plyr.setup(video.value)
-})
-
-console.log('Source: %s',source)
-console.log( 'Type: %d', props.slip.mediable.type)
 
 // if (!Hls.isSupported() || props.slip.mediable.type != 3) {
 //   console.log('HLS is not supported')
 //   videosource.value = source
 //   new Plyr(video, defaultOptions)
-// } else {
+// } else {}
 //   console.log('HLS is supported! :)')
 //   // For more Hls.js options, see https://github.com/dailymotion/hls.js
 //   console.log('Setting up HLS and load source')
@@ -115,7 +150,7 @@ console.log( 'Type: %d', props.slip.mediable.type)
     <!-- Video Wrapper -->
     <div class="w-4/6 flex flex-col justify-center items-center absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 rounded-2xl">
       <!--      <video ref="video" class="w-full rounded-xl" controls :src="slip.mediable.path" />-->
-      <video id="player" ref="video" preload="none" autoplay controls crossorigin />
+      <video id="player" ref="video" preload="none" autoplay controls crossorigin :src="source" />
       <div class="ambient-player">
         <canvas id="decoyVideo" class="decoy" />
       </div>
