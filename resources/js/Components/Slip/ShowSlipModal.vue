4c90757b6e0f50329c4e40c2177cd74d5930681c
ChrisToxz <chris@toxz.nl>
2023-07-08 16:59:26 +0200

    HLS Logic updated

--- resources/js/Components/Slip/ShowSlipModal.vue
+++ resources/js/Components/Slip/ShowSlipModal.vue
@@ -1,15 +1,16 @@
 <script setup>
 import Hls from 'hls.js'
 import plyr from 'plyr'
+import useCanvas from '@/Composables/useCanvas.js'
 import 'plyr/dist/plyr.css'
-import {onMounted, ref} from 'vue'
+import {isRef, onMounted, ref} from 'vue'
+
 
 const props = defineProps({
   slip: Object,
 })
 const video = ref(null)
-const src = props.slip.mediable.path
-
+const src = ref(props.slip.mediable.path)
 
 
 const defaultOptions = {
@@ -19,21 +20,32 @@ const defaultOptions = {
 onMounted(() => {
 
   if (!Hls.isSupported() || props.slip.mediable.type != 3) {
-    console.log('HLS is not supported')
+    console.log('%cHLS not supported, loading video!', 'color:green')
+
     new plyr(video.value, defaultOptions)
+
   } else if (Hls.isSupported()) {
+    console.log('%cHLS Supported, setting it up!', 'color:green')
     const hls = new Hls()
-    hls.loadSource(src)
+    hls.loadSource(isRef(src)? src.value : src)
     hls.attachMedia(video)
 
     hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
       const availableQualities = hls.levels.map((l) => l.height)
       availableQualities.unshift(0) //prepend 0 to quality array
+      console.log('%cQualities found: %s', 'color:green', availableQualities)
       defaultOptions.quality = {
         default: 0, //Default - AUTO
         options: availableQualities,
         forced: true,
-        onChange: (e) => updateQuality(e),
+        onChange: (e) => {
+          console.log(e)
+          updateQuality(e)},
+      }
+      defaultOptions.i18n = {
+        qualityLabel: {
+          0: 'Auto',
+        },
       }
       hls.on(Hls.Events.LEVEL_SWITCHED, function (event, data) {
         var span = document.querySelector('.plyr__menu__container [data-plyr=\'quality\'][value=\'0\'] span')
@@ -49,13 +61,14 @@ onMounted(() => {
     window.hls = hls
 
     function updateQuality(newQuality) {
+      console.log(newQuality)
       if (newQuality === 0) {
         window.hls.currentLevel = -1 //Enable AUTO quality if option.value = 0
-        console.log('Auto quality selection')
+        console.log('%cAuto quality selection', 'color:green')
       } else {
         window.hls.levels.forEach((level, levelIndex) => {
           if (level.height === newQuality) {
-            console.log('Found quality match with ' + newQuality)
+            console.log('%cFound quality match with ' + newQuality,'color:green')
             window.hls.currentLevel = levelIndex
           }
         })
