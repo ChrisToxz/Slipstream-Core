2745a704893abe8ed80b3b2454fd8a9086102b5e
Damion Voshall <126650414+DamoFD@users.noreply.github.com>
2023-07-11 12:53:29 -0700

    fix: Input models not binding to form (#28)
    * slip delete modal
    
    * fixed delete route
    
    * fixed upload inputs
    
    * fixed upload inputs

--- resources/js/Components/Reusable/PrimaryTextInput.vue
+++ resources/js/Components/Reusable/PrimaryTextInput.vue
@@ -1,3 +1,13 @@
+<script setup>
+const props = defineProps({
+  modelValue: String,
+})
+</script>
+
 <template>
-  <input class="w-full bg-brand-secondary-700 border border-black focus:border-brand-primary-500 focus:ring-0" type="text" />
+  <input
+    class="w-full bg-brand-secondary-700 border border-black focus:border-brand-primary-500 focus:ring-0" type="text"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
 </template>
