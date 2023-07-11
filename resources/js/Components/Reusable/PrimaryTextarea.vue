ea56c420f40be3ed25b587b97f100b6ea74c1516
DamoFD <voshall@yahoo.com>
2023-07-11 12:40:57 -0700

    fixed upload inputs

--- resources/js/Components/Reusable/PrimaryTextarea.vue
+++ resources/js/Components/Reusable/PrimaryTextarea.vue
@@ -1,3 +1,13 @@
+<script setup>
+const props = defineProps({
+  modelValue: String,
+})
+</script>
+
 <template>
-  <textarea class="w-full bg-brand-secondary-700 border-black focus:ring-0 focus:border-brand-primary-500" />
+  <textarea
+    class="w-full bg-brand-secondary-700 border-black focus:ring-0 focus:border-brand-primary-500"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
 </template>
