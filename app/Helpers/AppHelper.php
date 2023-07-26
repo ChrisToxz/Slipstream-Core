5fdd0b6c77a345dcd400954a323efa71535b24c9
ChrisToxz <chris@toxz.nl>
2023-07-26 20:27:00 +0200

    Finalize is_installed() helper function
    Might be refactored with a better, more advance check

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -3,23 +3,17 @@
 namespace App\Helpers;
 
 use App\Models\User;
-use Exception;
 
 class AppHelper
 {
 
     public static function is_installed()
     {
-
-        try {
-            $usersCount = User::count();
-
-            // If there are no users, redirect to the installation
-            if ($usersCount === 0) {
-                return redirect()->route('install');
-            }
-        } catch (Exception $e) {
-            return redirect()->route('install');
+        $usersCount = User::count();
+        // If there are no users, redirect to the installation
+        if ($usersCount === 0) {
+            return false;
         }
+        return true;
     }
 }
