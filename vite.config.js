3826a72dfc85de6d471e6d3a3627bd1df389a2d1
ChrisToxz <chris@toxz.nl>
2023-08-16 18:20:37 +0200

    chore(fix): Fix local NPM environment

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,34 @@
-import { defineConfig } from 'vite';
-import Icons from 'unplugin-icons/vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import {defineConfig} from 'vite'
+import Icons from 'unplugin-icons/vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-        Icons(),
-    ],
-});
+  plugins: [
+    laravel({
+      input: 'resources/js/app.js',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+    Icons(),
+  ],
+  server: {
+
+    https: false,
+
+    host: true,
+
+    port: 5173,
+
+    hmr: {host: 'localhost', protocol: 'ws'},
+
+  },
+})
+
